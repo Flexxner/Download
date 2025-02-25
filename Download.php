131469ec5cf271b58f9e5027747a0df5d76ca775
Flexxner <0_sitze_alleine@icloud.com>
2025-02-25 19:28:13 +0100

    Update Download.php

--- Download.php
+++ Download.php
@@ -1,6 +1,6 @@
 <?php
-$ordner = 'downloads'; // Ordner mit den Dateien
-$zipDatei = 'alle_dateien.zip';
+$ordner = 'downloads'; // Der Ordner mit den Dateien
+$zipDatei = 'downloads.zip';
 
 $zip = new ZipArchive;
 if ($zip->open($zipDatei, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) {
@@ -13,9 +13,12 @@ if ($zip->open($zipDatei, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) 
     $zip->close();
 }
 
+// iPhone-kompatible Header setzen
 header('Content-Type: application/zip');
 header('Content-Disposition: attachment; filename="' . $zipDatei . '"');
 header('Content-Length: ' . filesize($zipDatei));
+header("Pragma: no-cache");
+header("Expires: 0");
 readfile($zipDatei);
 
 // ZIP-Datei nach Download löschen
