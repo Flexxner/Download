f576a60abec7305f1cdce408915c7790033297f4
Flexxner <0_sitze_alleine@icloud.com>
2025-02-25 20:35:43 +0100

    Update Download.php

--- Download.php
+++ Download.php
@@ -1,24 +1,31 @@
 <?php
-$ordner = 'downloads'; // Ordner mit den Bildern
+$ordner = 'downloads'; // Der Ordner mit den Bildern
 $zipDatei = 'bilder.zip';
 
 $zip = new ZipArchive;
+
 if ($zip->open($zipDatei, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) {
-    if (isset($_POST['bilder'])) {
-        foreach ($_POST['bilder'] as $datei) {
-            $zip->addFile($ordner . '/' . $datei, $datei);
-        }
-    } elseif (isset($_GET['alle'])) {
-        $dateien = scandir($ordner);
-        foreach ($dateien as $datei) {
-            if ($datei != "." && $datei != "..") {
-                $zip->addFile($ordner . '/' . $datei, $datei);
+    if (isset($_GET['images'])) {
+        $bilder = explode(',', $_GET['images']);
+        if ($bilder[0] === 'all') {
+            // Alle Bilder hinzufügen
+            $dateien = scandir($ordner);
+            foreach ($dateien as $datei) {
+                if ($datei != "." && $datei != "..") {
+                    $zip->addFile($ordner . '/' . $datei, $datei);
+                }
+            }
+        } else {
+            // Ausgewählte Bilder hinzufügen
+            foreach ($bilder as $bild) {
+                $zip->addFile($bild);
             }
         }
     }
     $zip->close();
 }
 
+// ZIP-Datei zum Download anbieten
 header('Content-Type: application/zip');
 header('Content-Disposition: attachment; filename="' . $zipDatei . '"');
 header('Content-Length: ' . filesize($zipDatei));
